604d063eabd5461c8fc4a144a489eb3dd7c7190d
Daniele Bonaldo <dany.bony@gmail.com>
2016-06-24 18:54:10 +0100

    Swap looking direction every 500ms while looking for faces

--- app/src/main/java/net/bonysoft/magicmirror/facerecognition/LookingEyes.java
+++ app/src/main/java/net/bonysoft/magicmirror/facerecognition/LookingEyes.java
@@ -8,6 +8,10 @@ import net.bonysoft.magicmirror.R;
 
 public class LookingEyes extends ImageView {
 
+    private static final long LOOKING_DELAY_MILLIS = 500;
+
+    private LookingDirection lookingDirection;
+
     public LookingEyes(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
@@ -15,14 +19,46 @@ public class LookingEyes extends ImageView {
     public LookingEyes(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        setImageResource(R.drawable.eyes_left);
+        updateLookingDirection(LookingDirection.LEFT);
+    }
+
+    private void updateLookingDirection(LookingDirection direction) {
+        lookingDirection = direction;
+        if (direction == LookingDirection.LEFT) {
+            setImageResource(R.drawable.eyes_left);
+        } else {
+            setImageResource(R.drawable.eyes_right);
+        }
     }
 
     public void show() {
         setVisibility(VISIBLE);
+        getHandler().postDelayed(swapDirection, LOOKING_DELAY_MILLIS);
     }
 
     public void hide() {
+        getHandler().removeCallbacks(swapDirection);
         setVisibility(GONE);
+    }
+
+    private final Runnable swapDirection = new Runnable() {
+        @Override
+        public void run() {
+            updateLookingDirection(lookingDirection.next());
+            getHandler().postDelayed(this, LOOKING_DELAY_MILLIS);
+        }
+    };
+
+    private enum LookingDirection {
+        LEFT,
+        RIGHT;
+
+        public LookingDirection next() {
+            if (this == LEFT) {
+                return RIGHT;
+            } else {
+                return LEFT;
+            }
+        }
     }
 }
