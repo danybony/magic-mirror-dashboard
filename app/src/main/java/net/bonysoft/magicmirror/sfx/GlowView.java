ec21cb2d5a645c91101f105e957753a796bd1176
Daniele Bonaldo <dany.bony@gmail.com>
2016-06-24 15:23:28 +0100

    Convert GlowView to ImageView and display ImageBitmap gradient

--- app/src/main/java/net/bonysoft/magicmirror/sfx/GlowView.java
+++ app/src/main/java/net/bonysoft/magicmirror/sfx/GlowView.java
@@ -1,20 +1,25 @@
 package net.bonysoft.magicmirror.sfx;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.TransitionDrawable;
 import android.support.annotation.ColorInt;
 import android.support.annotation.ColorRes;
 import android.support.v4.util.SparseArrayCompat;
 import android.util.AttributeSet;
-import android.view.View;
+import android.widget.ImageView;
 
-public class GlowView extends View {
+public class GlowView extends ImageView {
 
     private static final int TRANSITION_DURATION = 700;
 
-    private final SparseArrayCompat<ColorDrawable> colors = new SparseArrayCompat<>();
+    private final SparseArrayCompat<BitmapDrawable> colors = new SparseArrayCompat<>();
 
     public GlowView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -23,30 +28,57 @@ public class GlowView extends View {
     public void transitionToColor(@ColorRes int colorRes) {
         // TODO timeout if too fast
         Drawable previousBackground = getPreviousDrawableSafely();
-        TransitionDrawable transitionDrawable = new TransitionDrawable(new Drawable[]{
-                previousBackground,
-                getOrCreateColorDrawableFor(colorRes)
-        });
-        setBackground(transitionDrawable);
+        TransitionDrawable transitionDrawable = createTransitionDrawable(previousBackground, colorRes);
+
+        setImageDrawable(transitionDrawable);
         transitionDrawable.startTransition(TRANSITION_DURATION);
     }
 
-    private ColorDrawable getOrCreateColorDrawableFor(@ColorRes int colorRes) {
-        ColorDrawable colorDrawable = colors.get(colorRes);
-        if (colorDrawable == null) {
-            colorDrawable = new ColorDrawable(getColor(colorRes));
-            colors.put(colorRes, colorDrawable);
-        }
-        return colorDrawable;
-    }
-
     private Drawable getPreviousDrawableSafely() {
-        Drawable previousDrawable = getBackground();
+        Drawable previousDrawable = getDrawable();
         if (previousDrawable == null) {
             int transparentRes = getColor(android.R.color.transparent);
             return new ColorDrawable(transparentRes);
+        } else {
+            TransitionDrawable previousTransitionDrawable = (TransitionDrawable) previousDrawable;
+            int idOfFrontDrawable = previousTransitionDrawable.getId(1);
+            return previousTransitionDrawable.findDrawableByLayerId(idOfFrontDrawable);
         }
-        return previousDrawable;
+    }
+
+    private TransitionDrawable createTransitionDrawable(Drawable previousBackground, @ColorRes int colorRes) {
+        return new TransitionDrawable(new Drawable[]{
+                previousBackground,
+                getOrCreateColorDrawableFor(colorRes)
+        });
+    }
+
+    private BitmapDrawable getOrCreateColorDrawableFor(@ColorRes int colorRes) {
+        BitmapDrawable bitmapDrawable = colors.get(colorRes);
+        if (bitmapDrawable == null) {
+            GradientDrawable gradientDrawable = new GradientDrawable();
+            gradientDrawable.setGradientType(GradientDrawable.RADIAL_GRADIENT);
+            gradientDrawable.setGradientRadius(getWidth() / 2);
+            gradientDrawable.setColors(new int[]{
+                    getColor(colorRes),
+                    Color.BLACK
+            });
+
+            Bitmap bitmap = drawableToBitmap(gradientDrawable);
+            bitmapDrawable = new BitmapDrawable(getResources(), bitmap);
+
+            colors.put(colorRes, bitmapDrawable);
+        }
+        return bitmapDrawable;
+    }
+
+    private Bitmap drawableToBitmap(Drawable drawable) {
+        Bitmap bitmap = Bitmap.createBitmap(getWidth(), getWidth(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
     }
 
     @ColorInt
